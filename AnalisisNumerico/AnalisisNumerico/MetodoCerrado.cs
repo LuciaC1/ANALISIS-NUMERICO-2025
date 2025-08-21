5e28420745cd55da4081f0d396cf9a53c48bc3bc
Guille Tagliavini <guillermina.tagliavini@gmail.com>
2025-08-21 19:53:24 -0300

    form

--- AnalisisNumerico/AnalisisNumerico/MetodoCerrado.cs
+++ AnalisisNumerico/AnalisisNumerico/MetodoCerrado.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using Calculus;
-usign Respuesta;
 
 
 namespace Unidad1
--- AnalisisNumerico/AnalisisNumerico/obj/Debug/net7.0/Unidad1.AssemblyInfo.cs
+++ AnalisisNumerico/AnalisisNumerico/obj/Debug/net7.0/Unidad1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Unidad1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61d9c0cb43fd21400cab1d90ce15d0d7f95380df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ff10c9c36da48b9742c3c2b851db1786d50180e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Unidad1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Unidad1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
