9f9fd4d8122fecdd0d05fcebedfef672490fe8e5
Lucia Copes <lucicopes@gmail.com>
2025-08-25 15:19:30 -0300

    Realizo idea formulario ingreso de datos

--- AnalisisNumerico/AnalisisNumerico/FormMenu.Designer.cs
+++ AnalisisNumerico/AnalisisNumerico/FormMenu.Designer.cs
@@ -28,19 +28,429 @@
         /// </summary>
         private void InitializeComponent()
         {
+            textBox3 = new TextBox();
+            label3 = new Label();
+            textBox4 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            label15 = new Label();
+            textBox12 = new TextBox();
+            label16 = new Label();
+            textBox13 = new TextBox();
+            textBox9 = new TextBox();
+            textBox10 = new TextBox();
+            textBox14 = new TextBox();
+            label10 = new Label();
+            textBox7 = new TextBox();
+            label11 = new Label();
+            label17 = new Label();
+            textBox15 = new TextBox();
+            label18 = new Label();
+            textBox1 = new TextBox();
+            Método = new ListBox();
             SuspendLayout();
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.FromArgb(233, 231, 235);
+            textBox3.BorderStyle = BorderStyle.FixedSingle;
+            textBox3.Location = new Point(45, 235);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(237, 23);
+            textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.FromArgb(177, 149, 160);
+            label3.BorderStyle = BorderStyle.FixedSingle;
+            label3.Font = new Font("Cascadia Mono", 12F);
+            label3.Location = new Point(107, 209);
+            label3.Name = "label3";
+            label3.Size = new Size(111, 23);
+            label3.TabIndex = 4;
+            label3.Text = "Iteraciones";
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = Color.FromArgb(233, 231, 235);
+            textBox4.BorderStyle = BorderStyle.FixedSingle;
+            textBox4.Location = new Point(45, 291);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(237, 23);
+            textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.FromArgb(177, 149, 160);
+            label4.BorderStyle = BorderStyle.FixedSingle;
+            label4.Font = new Font("Cascadia Mono", 12F);
+            label4.Location = new Point(116, 265);
+            label4.Name = "label4";
+            label4.Size = new Size(102, 23);
+            label4.TabIndex = 6;
+            label4.Text = "Tolerancia";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.FromArgb(177, 149, 160);
+            label5.BorderStyle = BorderStyle.FixedSingle;
+            label5.Font = new Font("Cascadia Mono", 12F);
+            label5.Location = new Point(116, 323);
+            label5.Name = "label5";
+            label5.Size = new Size(93, 23);
+            label5.TabIndex = 8;
+            label5.Text = "Intervalo";
+            // 
+            // textBox5
+            // 
+            textBox5.BackColor = Color.FromArgb(233, 231, 235);
+            textBox5.BorderStyle = BorderStyle.FixedSingle;
+            textBox5.Location = new Point(45, 349);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(105, 23);
+            textBox5.TabIndex = 9;
+            // 
+            // textBox6
+            // 
+            textBox6.BackColor = Color.FromArgb(233, 231, 235);
+            textBox6.BorderStyle = BorderStyle.FixedSingle;
+            textBox6.Location = new Point(177, 349);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(105, 23);
+            textBox6.TabIndex = 10;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.FromArgb(177, 149, 160);
+            label6.BorderStyle = BorderStyle.FixedSingle;
+            label6.Font = new Font("Cascadia Mono", 12F);
+            label6.Location = new Point(31, 385);
+            label6.Name = "label6";
+            label6.Size = new Size(129, 23);
+            label6.TabIndex = 11;
+            label6.Text = "xi , xInicial";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.FromArgb(177, 149, 160);
+            label7.BorderStyle = BorderStyle.FixedSingle;
+            label7.Font = new Font("Cascadia Mono", 12F);
+            label7.Location = new Point(211, 382);
+            label7.Name = "label7";
+            label7.Size = new Size(30, 23);
+            label7.TabIndex = 12;
+            label7.Text = "xd";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = Color.FromArgb(177, 149, 160);
+            label8.BorderStyle = BorderStyle.FixedSingle;
+            label8.Font = new Font("Cascadia Mono", 15F);
+            label8.Location = new Point(63, 38);
+            label8.Name = "label8";
+            label8.Size = new Size(206, 29);
+            label8.TabIndex = 13;
+            label8.Text = "Ingreso de datos";
+            label8.Click += label8_Click;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = Color.FromArgb(177, 149, 160);
+            label9.BorderStyle = BorderStyle.FixedSingle;
+            label9.Font = new Font("Cascadia Mono", 15F);
+            label9.Location = new Point(826, 38);
+            label9.Name = "label9";
+            label9.Size = new Size(134, 29);
+            label9.TabIndex = 27;
+            label9.Text = "Resultados";
+            label9.Click += label9_Click;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.BackColor = Color.FromArgb(177, 149, 160);
+            label12.BorderStyle = BorderStyle.FixedSingle;
+            label12.Font = new Font("Cascadia Mono", 12F);
+            label12.Location = new Point(758, 335);
+            label12.Name = "label12";
+            label12.Size = new Size(102, 23);
+            label12.TabIndex = 22;
+            label12.Text = "¿Converge?";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.BackColor = Color.FromArgb(177, 149, 160);
+            label13.BorderStyle = BorderStyle.FixedSingle;
+            label13.Font = new Font("Cascadia Mono", 12F);
+            label13.Location = new Point(836, 265);
+            label13.Name = "label13";
+            label13.Size = new Size(102, 23);
+            label13.TabIndex = 20;
+            label13.Text = "Tolerancia";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.BackColor = Color.FromArgb(177, 149, 160);
+            label14.BorderStyle = BorderStyle.FixedSingle;
+            label14.Font = new Font("Cascadia Mono", 12F);
+            label14.Location = new Point(793, 209);
+            label14.Name = "label14";
+            label14.Size = new Size(210, 23);
+            label14.TabIndex = 18;
+            label14.Text = "Iteraciones Realizadas";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.BackColor = Color.FromArgb(177, 149, 160);
+            label15.BorderStyle = BorderStyle.FixedSingle;
+            label15.Font = new Font("Cascadia Mono", 12F);
+            label15.Location = new Point(863, 148);
+            label15.Name = "label15";
+            label15.Size = new Size(66, 23);
+            label15.TabIndex = 16;
+            label15.Text = "Método";
+            // 
+            // textBox12
+            // 
+            textBox12.BackColor = Color.FromArgb(233, 231, 235);
+            textBox12.BorderStyle = BorderStyle.FixedSingle;
+            textBox12.Location = new Point(776, 110);
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(237, 23);
+            textBox12.TabIndex = 15;
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.BackColor = Color.FromArgb(177, 149, 160);
+            label16.BorderStyle = BorderStyle.FixedSingle;
+            label16.Font = new Font("Cascadia Mono", 12F);
+            label16.Location = new Point(857, 84);
+            label16.Name = "label16";
+            label16.Size = new Size(75, 23);
+            label16.TabIndex = 14;
+            label16.Text = "Función";
+            // 
+            // textBox13
+            // 
+            textBox13.BackColor = Color.FromArgb(233, 231, 235);
+            textBox13.BorderStyle = BorderStyle.FixedSingle;
+            textBox13.Location = new Point(776, 174);
+            textBox13.Name = "textBox13";
+            textBox13.Size = new Size(237, 23);
+            textBox13.TabIndex = 28;
+            // 
+            // textBox9
+            // 
+            textBox9.BackColor = Color.FromArgb(233, 231, 235);
+            textBox9.BorderStyle = BorderStyle.FixedSingle;
+            textBox9.Location = new Point(777, 235);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(237, 23);
+            textBox9.TabIndex = 29;
+            // 
+            // textBox10
+            // 
+            textBox10.BackColor = Color.FromArgb(233, 231, 235);
+            textBox10.BorderStyle = BorderStyle.FixedSingle;
+            textBox10.Location = new Point(777, 291);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(237, 23);
+            textBox10.TabIndex = 30;
+            // 
+            // textBox14
+            // 
+            textBox14.BackColor = Color.FromArgb(233, 231, 235);
+            textBox14.BorderStyle = BorderStyle.FixedSingle;
+            textBox14.Location = new Point(743, 361);
+            textBox14.Name = "textBox14";
+            textBox14.Size = new Size(139, 23);
+            textBox14.TabIndex = 31;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = Color.FromArgb(177, 149, 160);
+            label10.BorderStyle = BorderStyle.FixedSingle;
+            label10.Font = new Font("Cascadia Mono", 12F);
+            label10.Location = new Point(962, 335);
+            label10.Name = "label10";
+            label10.Size = new Size(48, 23);
+            label10.TabIndex = 35;
+            label10.Text = "Raíz";
+            label10.Click += label10_Click;
+            // 
+            // textBox7
+            // 
+            textBox7.BackColor = Color.FromArgb(233, 231, 235);
+            textBox7.BorderStyle = BorderStyle.FixedSingle;
+            textBox7.Location = new Point(777, 420);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(237, 23);
+            textBox7.TabIndex = 37;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.BackColor = Color.FromArgb(177, 149, 160);
+            label11.BorderStyle = BorderStyle.FixedSingle;
+            label11.Font = new Font("Cascadia Mono", 12F);
+            label11.Location = new Point(864, 395);
+            label11.Name = "label11";
+            label11.Size = new Size(57, 23);
+            label11.TabIndex = 36;
+            label11.Text = "Error";
+            label11.Click += label11_Click;
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.BackColor = Color.FromArgb(177, 149, 160);
+            label17.BorderStyle = BorderStyle.FixedSingle;
+            label17.Font = new Font("Cascadia Mono", 12F);
+            label17.Location = new Point(132, 148);
+            label17.Name = "label17";
+            label17.Size = new Size(66, 23);
+            label17.TabIndex = 40;
+            label17.Text = "Método";
+            // 
+            // textBox15
+            // 
+            textBox15.BackColor = Color.FromArgb(233, 231, 235);
+            textBox15.BorderStyle = BorderStyle.FixedSingle;
+            textBox15.Location = new Point(45, 110);
+            textBox15.Name = "textBox15";
+            textBox15.Size = new Size(237, 23);
+            textBox15.TabIndex = 39;
+            textBox15.TextChanged += textBox15_TextChanged;
+            // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.BackColor = Color.FromArgb(177, 149, 160);
+            label18.BorderStyle = BorderStyle.FixedSingle;
+            label18.Font = new Font("Cascadia Mono", 12F);
+            label18.Location = new Point(126, 84);
+            label18.Name = "label18";
+            label18.Size = new Size(75, 23);
+            label18.TabIndex = 38;
+            label18.Text = "Función";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.FromArgb(233, 231, 235);
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Location = new Point(915, 361);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(139, 23);
+            textBox1.TabIndex = 42;
+            // 
+            // Método
+            // 
+            Método.BackColor = Color.FromArgb(233, 231, 235);
+            Método.BorderStyle = BorderStyle.FixedSingle;
+            Método.FormattingEnabled = true;
+            Método.ItemHeight = 15;
+            Método.Items.AddRange(new object[] { "Bisección (cerrado)", "Regla Falsa (cerrado)", "Secante (abierto)", "Tangente (abierto)" });
+            Método.Location = new Point(45, 180);
+            Método.Name = "Método";
+            Método.SelectionMode = SelectionMode.MultiSimple;
+            Método.Size = new Size(237, 17);
+            Método.TabIndex = 43;
+            Método.SelectedIndexChanged += listBox1_SelectedIndexChanged;
             // 
             // FormMenu
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1009, 554);
+            BackColor = Color.FromArgb(200, 179, 193);
+            ClientSize = new Size(1094, 558);
+            Controls.Add(Método);
+            Controls.Add(textBox1);
+            Controls.Add(label17);
+            Controls.Add(textBox15);
+            Controls.Add(label18);
+            Controls.Add(textBox7);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(textBox14);
+            Controls.Add(textBox10);
+            Controls.Add(textBox9);
+            Controls.Add(textBox13);
+            Controls.Add(label9);
+            Controls.Add(label12);
+            Controls.Add(label13);
+            Controls.Add(label14);
+            Controls.Add(label15);
+            Controls.Add(textBox12);
+            Controls.Add(label16);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(label5);
+            Controls.Add(textBox4);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            ForeColor = Color.FromArgb(9, 10, 23);
             Name = "FormMenu";
-            Text = "Form1";
+            Text = "FormMenu";
             Load += FormMenu_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private TextBox textBox3;
+        private Label label3;
+        private TextBox textBox4;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Label label15;
+        private TextBox textBox12;
+        private Label label16;
+        private TextBox textBox13;
+        private TextBox textBox9;
+        private TextBox textBox10;
+        private TextBox textBox14;
+        private Label label10;
+        private TextBox textBox7;
+        private Label label11;
+        private Label label17;
+        private TextBox textBox15;
+        private Label label18;
+        private TextBox textBox1;
+        private ListBox Método;
     }
 }
--- AnalisisNumerico/AnalisisNumerico/FormMenu.cs
+++ AnalisisNumerico/AnalisisNumerico/FormMenu.cs
@@ -17,7 +17,57 @@ namespace U1
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
         private void FormMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox15_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
