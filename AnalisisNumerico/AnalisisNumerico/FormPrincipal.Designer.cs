259d80d9f9334848693883e87fa46b8e0d2067b9
Lucia Copes <lucicopes@gmail.com>
2025-08-27 16:09:21 -0300

    Idea de menu

---
+++ AnalisisNumerico/AnalisisNumerico/FormPrincipal.Designer.cs
@@ -0,0 +1,15 @@
+﻿namespace U1
+{
+    partial class FormPrincipal
+    {
+        private Panel panel1;
+        private Button buttonUnidad1;
+        private Panel panel2;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Button button4;
+        private Panel panel4;
+        private Panel panel3;
+    }
+}
